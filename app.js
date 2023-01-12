6ad6cfb80c9165c948611dd27396955608b936e2
Wrath <nu13drygon@gmail.com>
2023-01-12 10:27:54 -0600

    css fixes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const canvas = document.getElementById('canvas1')
-const canvasList = document.getElementsByClassName('canvas1')
 
-const ctx = canvas.getContext('2d')
+const canvasList = document.getElementsByClassName('canvas1')
+let canvasLength = canvasList.length
+
 
 const numberOfParticles = 10
-let particlesArray = []
-let particlesArray2 = [[], [], []]
+
+let particlesArray2 = []
+
+for (let i = 0; i < canvasLength; i++) {
+    particlesArray2.push([])
+}
 
 const srcArray = [
     'img/sw4.png',
@@ -15,35 +19,8 @@ const srcArray = [
 
 const picImage = new Image()
 picImage.src = srcArray[0]
-/*
-class Particle {
-    constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
-        this.size = Math.random() * 70 + 10
-        this.speed = Math.random() * 3 + 1
-        this.angle = Math.random() * 360
-        this.spin = Math.random() < 0.5 ?  -1 : 1
-    }
-    draw() {
-        ctx.save()
-        ctx.translate(this.x, this.y)
-        ctx.rotate(this.angle * Math.PI/360 * this.spin)
-        ctx.drawImage(picImage, 0 - this.size/2, 0 - this.size/2, this.size, this.size)
-        ctx.restore()
-    }
-    update() {
-        this.angle += 2
-        if(this.y - this.size > canvas.height) {
-            this.y = 0 - this.size
-            this.x = Math.random() * canvas.width
-            this.size = Math.random() * 70 + 10
-            this.speed = Math.random() * 3 + 1
-        }
-        this.y += this.speed
-    }
-}
-*/
+
+
 class Particle {
     constructor(canny) {
         this.canvas = canny;
@@ -75,49 +52,34 @@ class Particle {
 
 }
 
+init()
+animate()
+
+
 function init() {
     for (let i = 0; i < particlesArray2.length; i++) {
         for (let j = 0; j < numberOfParticles; j++) {
             particlesArray2[i].push(new Particle(canvasList[i]))
         }
     }
-    for (let j = 0; j < numberOfParticles; j++) {
-        particlesArray.push(new Particle(canvas))
-    }
 }
-init()
 
-function animate2(canny) {
+
+function animate() {
     for (let i = 0; i < particlesArray2.length; i++) {
         for (let j = 0; j < particlesArray2[i].length; j++) {
-            particlesArray2[i][j].ctx.clearRect(0, 0, canvas.width, canvas.height)
+            particlesArray2[i][j].ctx.clearRect(0, 0, particlesArray2[i][j].canvas.width, particlesArray2[i][j].canvas.height)
         }
     }
     for (let i = 0; i < particlesArray2.length; i++) {
         for (let j = 0; j < particlesArray2[i].length; j++) {
             
             particlesArray2[i][j].draw()
-        
+            
             particlesArray2[i][j].update()
             
         }
     }
-    requestAnimationFrame(animate2)
-}
-
-function animate(canny) { 
-    // ctx.clearRect(0, 0, canvas.width, canvas.height)
-    for (let i = 0; i < particlesArray.length; i++) {
-        particlesArray[i].ctx.clearRect(0, 0, canvas.width, canvas.height)
-    }
-    for (let i = 0; i < particlesArray.length; i++) {
-        
-        particlesArray[i].draw()
-        
-        particlesArray[i].update()
-    }
     requestAnimationFrame(animate)
 }
 
-animate2()
-// animate2()
